79c9f4a34767015b571d16fe17a2252b68b8fb58
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2014-02-17 22:35:48 +0100

    Improve ADFS compatibility: format the timestamp with timezone and prepend uuid with underscore.

--- onelogin/saml/AuthRequest.py
+++ onelogin/saml/AuthRequest.py
@@ -47,10 +47,10 @@ def create(
     # http://docs.oasis-open.org/security/saml/v2.0/saml-core-2.0-os.pdf Section
     # 1.3.3
     now = now.replace(microsecond=0)
-    now_iso = now.isoformat()
+    now_iso = now.isoformat() + "Z"
 
     unique_id = _uuid()
-    unique_id = unique_id.hex
+    unique_id = '_' + unique_id.hex[:-1]
 
     samlp_maker = ElementMaker(
         namespace='urn:oasis:names:tc:SAML:2.0:protocol',
